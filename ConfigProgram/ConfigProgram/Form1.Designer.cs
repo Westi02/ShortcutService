41706de1dae02eb59d3f00d3b8ed5dfc740632d6
Florian Westphal <westphal.f3@gmail.com>
2023-07-05 21:38:58 +0200

    ConfigProgram Project created

---
+++ ConfigProgram/ConfigProgram/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ConfigProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_load = new Button();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // btn_load
+            // 
+            btn_load.Location = new Point(12, 12);
+            btn_load.Name = "btn_load";
+            btn_load.Size = new Size(75, 23);
+            btn_load.TabIndex = 0;
+            btn_load.Text = "load config";
+            btn_load.UseVisualStyleBackColor = true;
+            btn_load.Click += btn_load_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "tets", "test", "test2" });
+            comboBox1.Location = new Point(132, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1050, 718);
+            Controls.Add(comboBox1);
+            Controls.Add(btn_load);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Shortcut Service Config";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btn_load;
+        private ComboBox comboBox1;
+    }
+}
---
+++ ConfigProgram/ConfigProgram/Form1.cs
@@ -0,0 +1,20 @@
+namespace ConfigProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
