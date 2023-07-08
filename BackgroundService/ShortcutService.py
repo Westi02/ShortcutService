7e1082a50d98d8fbf95eb85e0c6bfbc46019ddcf
Mukade <lucas.baaske@gmx.de>
2023-07-08 22:22:49 +0200

    Backgroundservice optimization

--- BackgroundService/ShortcutService.py
+++ BackgroundService/ShortcutService.py
@@ -1,28 +1,32 @@
-import keyboard # for keylogs
+import keyboard 
 import json
 import subprocess
-import os
 from win32com.shell import shell, shellcon
 
+class Keylogger:
+    def readJson(self):
+        # JSON file
+        path = f"{shell.SHGetFolderPath(0, shellcon.CSIDL_PERSONAL, None, 0)}\shortcut_config.json"
+        f = open (path, "r")
 
-def readJson():
-    # JSON file
-    path = f"{shell.SHGetFolderPath(0, shellcon.CSIDL_PERSONAL, None, 0)}\shortcut_config.json"
-    f = open (path, "r")
+        # Reading from file
+        self.configdata = json.loads(f.read())
 
-    # Reading from file
-    data = json.loads(f.read())
+        # Closing file
+        f.close()
 
-    # Closing file
-    f.close()
-
-    return data
-
-if __name__ == "__main__":
-    data = readJson()
-    while True:
-        for Shortcut in data['Shortcuts']:
+    def callback(self, event):
+        for Shortcut in self.configdata['Shortcuts']:
             if not Shortcut["ShortcutString"] == "":
                 if keyboard.is_pressed(Shortcut["ShortcutString"]):
                     if not Shortcut["ProgramPath"] == "":
                         subprocess.call(Shortcut['ProgramPath'],shell=True)
+
+    def start(self):
+        self.readJson()
+        keyboard.on_press(callback=self.callback)
+        keyboard.wait()
+
+if __name__ == "__main__":
+    keylogger = Keylogger()
+    keylogger.start()
